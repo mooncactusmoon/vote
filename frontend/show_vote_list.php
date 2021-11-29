99b7e34879e322773a7c04e9dc0890de7ba766f1
MoonLin <mooncactusmoon@gmail.com>
2021-11-29 14:52:04 +0800

    美化題目列表

--- frontend/show_vote_list.php
+++ frontend/show_vote_list.php
@@ -1,14 +1,27 @@
 <h1>列出所有的問題</h1>
 <?php
 $subjects=all('topics');
-echo "<ol>";
+echo "<ol class='list-group'>";
 foreach ($subjects as $key => $value) {
     if(rows('options',['topic_id'=>$value['id']]) > 0){
-    echo "<li>";
+    echo "<li class='list-group-item'>";
     // ?代表當前頁面
-    echo "<a href='index.php?do=vote&id={$value['id']}'>";
+    // 題目
+    echo "<a class='d-inline-block col-md-8' href='index.php?do=vote&id={$value['id']}'>";
     echo $value['topic'];
     echo "</a>";
+    //總投票數顯示
+    $count=q("select sum(`count`) as 'total' from `options` where `topic_id`='{$value['id']}'");
+    
+    echo "<span class='d-inline-block col-md-2 '>";
+    echo "total : ". $count[0]['total'] ;
+    echo "</span>";
+    
+    //看結果按鈕
+    echo "<span class='d-inline-block col-md-2 text-center'>";
+    echo "<button class='btn-info'>觀看結果</button>";
+    echo "</span>";
+
     echo "</li>";
 }
 }
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>問卷系統</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <style>
+    <style>       
         body{
             background-color: #f2f5f5;
         }
