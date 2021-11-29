ee8af11984b470d9d4916d32626f3cd0cc3fb040
MoonLin <mooncactusmoon@gmail.com>
2021-11-29 09:07:46 +0800

    用include前的樣子

--- api/db.php
+++ api/db.php
@@ -3,82 +3,104 @@
 $dsn="mysql:host=localhost;charset=utf8;dbname=vote";
 $pdo=new PDO($dsn,'root','');
 
-function find($table,$id){
+ //取得符合條件的一筆資料
+ function find($table,$id){
     global $pdo;
-    $sql="SELECT * FROM `$table` WHERE `id`='$id'";
-    
-    return $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);    
-}
-   //取出指定資料表的所有資料
-function all($table){
-    global $pdo;
-    $sql="SELECT * FROM `$table`";
-    $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    return $rows;
-    //return $pdo->query($sql)->fetchAll();
-}
-/**
- * update 函式設計
- * 1.指定資料表 $table => 字串
- * 2.指定更新的欄位 $column => 陣列
- * 3.指定更新的條件
- */
+    $sql="SELECT * FROM `$table` WHERE ";
 
-$column=['date'=>'2021-11-22',
-         'place'=>'職訓中心'];
-$where=['payway'=>'現金',
-        'item'=>'午餐'];
+    if(is_array($id)){
+        foreach($id as $key=>$value){
+            $tmp[]="`$key`='$value'";
+        }
         
-update('detail',$column,$where);
-/*  $sql="";
-foreach ($array as $key => $value) {
-    echo '迴圈執行前=> '.$sql.'<br>';
-    $sql=$sql . "`$key`='$value' AND ";
-    echo '迴圈執行後=> '.$sql.'<br>';
+        $sql=$sql. implode(" AND ",$tmp);
+    }else{
+       $sql=$sql . "`id`='$id'";
+    }
+
+    return $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
 }
-echo "迴圈完成後=>".mb_substr($sql,0,mb_strlen($sql)-5); */
- function update($table,$column,$where){
-    global $pdo;
 
-    $sql_set='';
-    foreach ($column as $key => $value) {
-        $sql_set=$sql_set . "`$key`='$value',";
+
+
+//取出指定資料表的所有資料
+function all($table,...$arg){
+global $pdo;
+$sql="SELECT * FROM `$table` ";
+if(isset($arg[0])){
+    if(is_array($arg[0])){
+        foreach($arg[0] as $key=>$value){
+            $tmp[]="`$key`='$value'";
+        }
+        
+        $sql=$sql."where " . implode(" AND ",$tmp);
+    }else{
+        $sql=$sql.$arg[0];
     }
-    $sql_set=trim($sql_set,',');
+}
 
-    $sql_where='';
-    foreach ($where as $key => $value) {
-        $sql_where=$sql_where . "`$key`='$value' AND ";
-    }
-    $sql_where=mb_substr($sql_where,0,mb_strlen($sql_where)-5);;
+if(isset($arg[1])){
+    $sql=$sql.$arg[1];
+}
 
-    mb_substr($sql_where,0,mb_strlen($sql_where)-5);
-    $sql="UPDATE `$table` SET $sql_set WHERE $sql_where ";
-    "UPDATE `expenditure` 
-        SET `date`='2021-11-22',`place`='職訓中心' 
-         WHERE `payway`='現金' AND `item`='午餐'";
-    //echo $sql;
-    $pdo->exec($sql);
+//echo $sql;
 
- }
+$rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+return $rows;
+//return $pdo->query($sql)->fetchAll();
+}
+
+
+function update($table,$column,$where){
+global $pdo;
+
+$sql_set='';
+foreach ($column as $key => $value) {
+    $sql_set=$sql_set . "`$key`='$value',";
+}
+$sql_set=trim($sql_set,',');
+
+$sql_where='';
+foreach ($where as $key => $value) {
+    $sql_where=$sql_where . "`$key`='$value' AND ";
+}
+$sql_where=mb_substr($sql_where,0,mb_strlen($sql_where)-5);;
+
+mb_substr($sql_where,0,mb_strlen($sql_where)-5);
+$sql="UPDATE `$table` SET $sql_set WHERE $sql_where ";
+//echo $sql;
+$pdo->exec($sql);
+
+}
+
+
 function insert($table,$array){
-    global $pdo;
-    // $array=['id'=>'1',
-    //         'subject'=>'今天要吃甚麼'];
-   
-    $sql="INSERT into $table(`" . implode('`,`',array_keys($array)) . "`) 
-                       value('" . implode("','",$array) ."')";
+ global $pdo;
 
-    echo $sql;
-    // return $pdo->exec($sql);
+
+ $sql="INSERT into $table(`" . implode('`,`',array_keys($array)) . "`) 
+                    value('" . implode("','",$array) ."')";
+
+echo $sql."<br>";
+return $pdo->exec($sql);
+
 }
 
 
- function del($table,$id){
-     global $pdo;
-     $sql="DELETE FROM `$table` WHERE `id`='$id'";
 
-     return $pdo->exec($sql);
- }
+function del($table,$id){
+global $pdo;
+$sql="DELETE FROM `$table` WHERE ";
+if(is_array($id)){
+    foreach($id as $key=>$value){
+        $tmp[]="`$key`='$value'";
+    }
+    
+    $sql=$sql. implode(" AND ",$tmp);
+}else{
+   $sql=$sql . "`id`='$id'";
+}
+return $pdo->exec($sql);
+}
 
- ?>
+?>
