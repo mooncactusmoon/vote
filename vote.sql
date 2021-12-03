ba52e8bb25c79a2bb8838049abf71161808237aa
MoonLin <mooncactusmoon@gmail.com>
2021-12-03 16:39:04 +0800

    新增sql檔案

--- vote.sql
+++ vote.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2021-11-29 08:07:47
+-- 產生時間： 2021-12-03 09:38:14
 -- 伺服器版本： 10.4.21-MariaDB
 -- PHP 版本： 7.4.24
 
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `ad`
+--
+
+CREATE TABLE `ad` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(64) COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '檔名',
+  `sh` tinyint(1) UNSIGNED NOT NULL COMMENT '上架',
+  `intro` varchar(64) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL COMMENT '備註'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 傾印資料表的資料 `ad`
+--
+
+INSERT INTO `ad` (`id`, `name`, `sh`, `intro`) VALUES
+(3, '1.jpg', 1, '藍月輪');
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `options`
 --
 
@@ -39,12 +59,29 @@ CREATE TABLE `options` (
 --
 
 INSERT INTO `options` (`id`, `opt`, `count`, `topic_id`) VALUES
-(1, '吃飯', 4, 1),
-(2, '吃麵', 1, 1),
-(7, '晴天', 0, 3),
-(8, '123', 0, 3),
-(9, '456', 0, 3),
-(10, '0', 0, 3);
+(7, '晴天123', 2, 3),
+(12, '雨天', 1, 3),
+(13, '陰天', 1, 3),
+(14, '未知QQ', 2, 3),
+(15, 'ˋ W ˊ', 0, 5),
+(16, 'OWO', 0, 5),
+(17, 'QWQ', 1, 5),
+(18, '~ W ~', 0, 5),
+(24, '呱呱', 0, 6),
+(25, '嘿嘿', 0, 6),
+(26, '哈哈', 0, 6),
+(27, '嘟嘟', 0, 6),
+(28, 'A', 0, 7),
+(29, 'B', 0, 7),
+(30, 'C', 0, 7),
+(31, '11', 0, 8),
+(32, '22', 0, 8),
+(33, '33', 0, 8),
+(34, '000A', 0, 9),
+(35, '000B', 0, 9),
+(36, '000C', 0, 9),
+(37, '000D', 0, 9),
+(38, '000E', 0, 9);
 
 -- --------------------------------------------------------
 
@@ -62,13 +99,45 @@ CREATE TABLE `topics` (
 --
 
 INSERT INTO `topics` (`id`, `topic`) VALUES
-(1, '吃啥咪'),
-(2, 'AAA'),
-(3, '天氣');
+(3, '天氣'),
+(5, '最新測試問卷'),
+(6, '測試問卷2'),
+(7, '1203test'),
+(8, 'test22'),
+(9, '0000');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `account` varchar(24) COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '帳號',
+  `password` varchar(24) COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '密碼',
+  `email` varchar(64) COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '信箱',
+  `name` varchar(12) COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '姓名',
+  `gender` varchar(2) COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '性別',
+  `birthday` date NOT NULL COMMENT '生日'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 傾印資料表的資料 `users`
+--
+
+INSERT INTO `users` (`id`, `account`, `password`, `email`, `name`, `gender`, `birthday`) VALUES
+(0, 'moon', '1234', 'moon@gmail', 'MoonLin', '女', '2021-11-02');
 
 --
 -- 已傾印資料表的索引
 --
+
+--
+-- 資料表索引 `ad`
+--
+ALTER TABLE `ad`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- 資料表索引 `options`
@@ -83,20 +152,32 @@ ALTER TABLE `topics`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `ad`
+--
+ALTER TABLE `ad`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `options`
 --
 ALTER TABLE `options`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `topics`
 --
 ALTER TABLE `topics`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
