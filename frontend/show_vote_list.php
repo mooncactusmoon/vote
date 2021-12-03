77f61832cda6e08b2051ae1340e7f7bbc8548de8
MoonLin <mooncactusmoon@gmail.com>
2021-12-03 13:26:25 +0800

    禁止非登入會員投票

--- frontend/show_vote_list.php
+++ frontend/show_vote_list.php
@@ -1,4 +1,4 @@
-<h1>列出所有的問題</h1>
+<h1>列出所有的問卷</h1>
 <?php
 $subjects=all('topics');
 echo "<ol class='list-group'>";
@@ -6,10 +6,15 @@ foreach ($subjects as $key => $value) {
     if(rows('options',['topic_id'=>$value['id']]) > 0){
     echo "<li class='list-group-item'>";
     // ?代表當前頁面
-    // 題目
-    echo "<a class='d-inline-block col-md-8' href='index.php?do=vote&id={$value['id']}'>";
-    echo $value['topic'];
-    echo "</a>";
+    // 題目 (判斷登入的會員才能使用投票功能)
+    if(isset($_SESSION['user'])){
+
+        echo "<a class='d-inline-block col-md-8' href='index.php?do=vote&id={$value['id']}'>";
+        echo $value['topic'];
+        echo "</a>";
+    }else{
+        echo "<span class='d-inline-block col-md-8'>".$value['topic']."</span>";
+    }
     //總投票數顯示
     $count=q("select sum(`count`) as 'total' from `options` where `topic_id`='{$value['id']}'");
     
--- frontend/vote.php
+++ frontend/vote.php
@@ -7,23 +7,23 @@ $options=all('options',['topic_id'=>$id]);
 
 ?>
 <h1><?=$subject['topic'];?></h1>
-<ol>
+<ol class="list-group">
 <form action="./api/save_vote.php" method="post">
 
     <?php
     foreach ($options as $key => $opt){
         if($opt['opt']!=""){
 
-            echo "<li>";
+            echo "<label class='list-group-item  list-group-item-action'>";
             //多選的話 name='opt[]'  單選擇可以不用加[]
             echo "<input type='radio' name='opt' value='{$opt['id']}'>";
             echo $opt['opt'];    
-            echo "</li>";    
+            echo "</label>";    
         }
     }
     
     ?>
 
 </ol>
-<input type="submit" value="投票">
+<input type="submit" value="投票" class="btn btn-info m-3">
 </form>
