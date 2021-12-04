19bca0632190262a8a35fb131bedc0623296c327
Moon <mooncactusmoon@gmail.com>
2021-12-05 00:46:36 +0800

    後台樣板也更新

--- backend/index.php
+++ backend/index.php
@@ -15,24 +15,43 @@ if(!isset($_SESSION['user'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>問卷系統</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <style>
-        .container{
-            min-height: 596px;
-        }
-    </style>
+    <style>  
+      
+      body{
+          background-color: #f2f5f5;
+      }
+
+      header{
+          height: 200px;
+      }
+      .ad{
+          background-color: #CCC;
+      }
+      .member{
+          background-color:#0b1f5c;
+          color: #475FA8;
+          font-size: 20px;
+      }
+
+      footer{
+          position: fixed;
+          bottom: 0px;
+          width: 1110px;
+      }
+  </style>
 </head>
 
 <body>
 
 <!-- 輪播  用陣列去顯示圖片-->
 
-<div class="jumbotron p-0 mb-0" style="overflow:hidden;height:200px;">
-<a href="index.php">
+    <!-- <div class="jumbotron p-0 mb-0" style="overflow:hidden;height:200px;">
+    <a href="index.php">
     <div id="carouselExampleIndicators" class="carousel slide position-relative" data-ride="carousel">
         <div class="carousel-inner position-absolute" style="top:-250px;">
-<?php
-$images=all('ad',['sh'=>1]);
-foreach($images as $key => $image){
+    <?php
+    $images=all('ad',['sh'=>1]);
+    foreach($images as $key => $image){
     if($key==0){
 
         echo "<div class='carousel-item active'>";
@@ -41,10 +60,10 @@ foreach($images as $key => $image){
 
     }
     echo "<img src='../img/{$image['name']}' alt='{$image['intro']}' class='d-block w-100'></div>";
-}
+    }
 
 
-?>      
+    ?>      
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -55,12 +74,12 @@ foreach($images as $key => $image){
             <span class="sr-only">Next</span>
         </a>
     </div>
-</a>
-</div>
+    </a>
+    </div> -->
 <!-- 輪播 end -->
 
 <!-- nav -->
-<nav class="bg-light shadow-sm py-3 px-2 text-right mb-4 d-flex justify-content-between">
+<nav class="bg-light shadow-sm py-3 px-2 text-right my-0 d-flex justify-content-between">
     <div>
         <!-- 放置功能 -->
         <a class="px-2" href="?do=show_vote_list">問卷管理</a>
@@ -81,8 +100,33 @@ if(isset($_SESSION['user'])){
 </nav>
 <!-- nav end -->
 
+<main class="d-flex row  container mx-auto">
+<!-- header -->
+<header class="container bg-warning">
+    <div class="row">
 
-<div class="container">
+        I am Header
+    </div>
+</header>
+<!-- header end -->
+
+
+
+
+<!-- main 包含了 aside(sidebar) section -->
+
+
+
+<!-- sidebar -->
+<aside class="container col-3 member">
+HIHI
+</aside>
+<!-- sidebar end -->
+
+
+<!-- section -->
+
+<section class="container col-6">
 
     <?php
     //判斷有沒有傳值，來決定顯現畫面
@@ -95,11 +139,26 @@ if(isset($_SESSION['user'])){
     }
     
     ?>
-</div>
+</section>
+<!-- section end -->
 
-<div class="p-3 text-center text-light bg-dark">
-    &copy;cacuts月
+
+
+<!-- ad bar -->
+<div class="container col-3 ad ">
+AD
 </div>
+<!-- ad bar end -->
+
+
+</main>
+<!-- main end -->
+
+<!-- footer -->
+<footer class="py-3 text-center text-light bg-dark container-fluid">
+    &copy;Cacuts月
+</footer>
+<!-- footer end -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
