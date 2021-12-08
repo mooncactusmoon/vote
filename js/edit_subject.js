757877af5bedef2d4afed575ebe7933eaebe3146
Moon <mooncactusmoon@gmail.com>
2021-12-08 23:03:26 +0800

    初步增減JS選項完成 (尚未連接資料庫)

--- js/edit_subject.js
+++ js/edit_subject.js
@@ -1,11 +1,22 @@
 //增加選項的input
 function add_opt(){
     //用clone的方式增加option的input
-    let opt_h=document.getElementById('opt_h').cloneNode();
     let opt_t=document.getElementById('opt_t').cloneNode();
+    let opt_h=document.getElementById('opt_h').cloneNode();
     document.getElementById('div').appendChild(opt_t);
     //刪除多餘的input ?
     //限制input個數 ?
 
-}
-            
+    let but=document.createElement('button');
+    but.style="font-size:18px";
+    but.className="bg-warning rounded";
+    but.innerHTML="-";
+    but.type='button';
+    but.name='lol'; 
+    but.onclick = function(){
+        div.removeChild(but.previousElementSibling);
+        div.removeChild(but);
+        
+    }
+    document.getElementById('div').appendChild(but);
+}     
