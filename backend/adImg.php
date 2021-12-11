2b70ec9c8417a5e7b965011687c725b9c7241394
Moon <mooncactusmoon@gmail.com>
2021-12-11 23:55:06 +0800

    廣告區更新完畢

--- backend/adImg.php
+++ backend/adImg.php
@@ -1,43 +1,46 @@
-<!-- 廣告管理 -->
+<!-- 後台:廣告管理 -->
 <?php
     if(isset($_SESSION['upload_ok'])){
         echo "<script>alert('{$_SESSION['upload_ok']}')</script>";
         unset($_SESSION['upload_ok']);
     }
 ?>
-<h2 class="text-center mt-3 font-weight-bold">廣告圖片管理</h2>
+<div class="container">
 
-<div class="row ">
-    <div class="col-md-3 input-group">
-        <div class="row">
-            <form action="../api/upload_ad.php" method="post" enctype="multipart/form-data" >        
-                <div class="custom-file d-block m-auto row">
-                    <label for="upload" class="custom-file-label row">選擇檔案</label>
-                    <input type="file" name="name" id="upload" class="row custom-file-input " >
-                </div>
-                <div class="text-center input-group m-auto row">    
-                    <input type="text" name="intro" id="intro" class="form-control" placeholder="輸入說明" required>
-                </div>
-                <div class="text-center mt-3 m-auto">
-                    <input type="submit" value="上傳" class="btn btn-info">
-                </div>    
-                
-            </form>
+    <h2 class="text-center my-3 font-weight-bold">廣告圖片管理</h2>
+    
+    <div class="row">
+        <div class="input-group ml-5">
+            <div class="row">
+                <form action="../api/upload_ad.php" method="post" enctype="multipart/form-data" >        
+                    <div class="custom-file d-block m-auto row">
+                        <label for="upload" class="custom-file-label row">選擇檔案</label>
+                        <input type="file" name="name" id="upload" class="row custom-file-input " >
+                    </div>
+                    <div class="text-center input-group m-auto row">    
+                        <input type="text" name="intro" id="intro" class="form-control" placeholder="輸入說明" required>
+                    </div>
+                    <div class="text-center mt-3 m-auto">
+                        <input type="submit" value="上傳" class="btn btn-info">
+                    </div>    
+                    
+                </form>
+            </div>
+            
         </div>
-
     </div>
-    <div class="col-md-9">
-        <h3 class="text-center my-3">圖片列表</h3>
-        <p class="text-center text-danger">建議廣告擺放不超過三張</p>
-        <table class="table">
-            <tr>
-                <td>圖片</td>
-                <td>說明</td>
-                <td>狀態</td>
-                <td>管理</td>
-            </tr>
-
-        <?php
+        <div class="row">
+            <h3 class="text-center font-weight-bold m-3">圖片列表</h3>
+            <p class="text-danger my-4">建議廣告擺放不超過三張</p>
+            <table class="table">
+                <tr class="font-weight-bold" style="color:#2b537a;">
+                    <td>圖片</td>
+                    <td>說明</td>
+                    <td>狀態</td>
+                    <td>管理</td>
+                </tr>
+                
+                <?php
         $rows=all('ad');
         foreach($rows as $row){
             echo "<tr>";
@@ -45,10 +48,10 @@
             // 圖片縮小先寫死
             echo "<img src='../img/{$row['name']}' style='width:100px;height:75px;'>";
             echo "</td>";
-
+            
             echo "<td>{$row['intro']}</td>";
             echo "<td>";
-
+            
             echo "<a href='../api/change_status.php?id={$row['id']}'>";
             echo ($row['sh']==1)?"顯示中":"未上架";
             echo "</a></td>";
@@ -57,11 +60,12 @@
             echo "<a class='btn btn-info' href='?do=edit_ad&id={$row['id']}'>修改</a>";
             echo "<a class='btn btn-danger' href='../api/del_ad.php?id={$row['id']}'>刪除</a>";
             echo "</td>";
-
+            
             echo "</tr>";
             
         }
         ?>
         </table>
     </div>
+
 </div>
--- backend/edit_ad.php
+++ backend/edit_ad.php
@@ -1,27 +1,29 @@
+<!-- 修改編輯個別廣告圖 -->
 <?php include_once "../api/db.php"; ?>
-<h2 class="text-center mt-3 font-weight-bold">修改輪播圖</h2>
+
+<h2 class="text-center mt-3 font-weight-bold">修改廣告圖</h2>
 <?php
 $ad=find('ad',$_GET['id']);
 
 ?>
-<div class="col-md-3 m-auto">
+<div class="m-auto">
     <img src="../img/<?=$ad['name'];?>" style="width:250px;">
-
     
     <form action="../api/edit_ad.php" method="post" enctype="multipart/form-data">
         
-        <div class="custom-file d-block m-auto ">
+        <div class="custom-file d-block mx-auto my-2" style="width:350px;">
             <label for="upload" class=" custom-file-label "><?=$ad['name'];?></label>
             <input type="file" name="name" id="upload" class=" custom-file-input " >
         </div>
-        <div class="text-center input-group m-auto">
+        <div class="text-center input-group mx-auto my-2" style="width:350px;">
+            <input type="text" name="intro" id="intro" class="form-control" value="<?=$ad['intro'];?>" required>
             <label for="intro" class="input-group-prpend input-group-text">說明</label>
-            <input type="text" name="intro" id="intro" class="form-control" value="<?=$ad['intro'];?>">
         </div>
         <div class="text-center mt-3 m-auto">
             <input type="hidden" name="id" value="<?=$ad['id'];?>">
-            <input type="submit" value="上傳" class="btn btn-info">
+            <input type="submit" value="修改" class="btn btn-info">
         </div>    
         
     </form>
+
 </div>
