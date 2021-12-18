c3a7b47ff991d19956ce6702fa16ee3de3e648b6
Moon <mooncactusmoon@gmail.com>
2021-12-19 02:41:28 +0800

    註冊帳號完成 可防止帳號及信箱重複及空白資料

--- api/reg.php
+++ api/reg.php
@@ -1,10 +1,37 @@
 <?php include_once "db.php"; ?>
 <?php
-//由於frontend/reg.php的name都跟資料表有對到，故可以直接用$_POST來取值
-insert('users',$_POST);
-echo insert('users',$_POST);
 
-$_SESSION['success']="會員註冊成功，可以嘗試登入了";
-to("../index.php");
+$sql1="SELECT * FROM users Where account = '{$_POST["account"]}'";
+$sql2="SELECT * FROM users Where email = '{$_POST["email"]}'";
+$result=$pdo->query($sql1)->fetch();
+$result2=$pdo->query($sql2)->fetch();
+
+
+
+if($result!=0 && $result2 ==0){
+
+    echo "<script>";
+    echo "alert('帳號已有人使用過，請使用其他帳號');";
+    echo "history.back();";
+    echo "</script>";
+}else if($result == 0 && $result2 !=0){
+    echo "<script>";
+    echo "alert('信箱已被註冊，請使用其他信箱');";
+    echo "history.back();";
+    echo "</script>";
+}else if($result !=0 && $result2 !=0){
+    echo "<script>";
+    echo "alert('【帳號】及【信箱】都有人使用過，請使用其他【帳號】及【信箱】');";
+    echo "history.back();";
+    echo "</script>";
+
+}else{
+    
+    //由於frontend/reg.php的name都跟資料表有對到，故可以直接用$_POST來取值
+    insert('users',$_POST);
+    $_SESSION['success']="會員註冊成功，可以嘗試登入了";
+    to("../index.php");
+}
+ 
 
 ?>
--- frontend/reg.php
+++ frontend/reg.php
@@ -3,34 +3,85 @@
     <h2 class="text-center text-light pt-2"><i class="far fa-address-book">&nbsp;&nbsp;註冊會員</i></h2>
 </div>
 <div class="container" style="height:508px">
-<form action="./api/reg.php" method="post" id="regForm" class="text-center">
+<form action="./api/reg.php" method="post" name="myForm" class="text-center">
     <div class="form-group row mt-4">
-        <label for="account" class="col-sm-4 col-form-label text-center "><i
-        class="fas fa-file-signature">&nbsp;&nbsp;帳號</i></label>
+        <div class="col-6">
+            <div class="row">
+
+                <label for="account" class="col-sm-4 col-form-label text-center "><i
+                class="fas fa-file-signature">&nbsp;&nbsp;帳號</i></label>
+                <div class=" col-sm-8">
+                    <div class="row">
+                        <input type="text" class="form-control border-info" id="account" name="account" placeholder="請輸入至少8個字元" pattern="[a-zA-Z0-9]{8,}">
+                    </div>
+                    <div class="row">
+                        <p id="acc_p" >&nbsp;</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-6">
+            <div class="row">
+            <label for="name" class="col-sm-4 col-form-label text-center">
+            <i class="fas fa-user-tie">&nbsp;&nbsp;姓名</i></label>
         <div class=" col-sm-8 ">
-            <input type="text" class="form-control border-info" id="account" name="account" placeholder="請輸入至少8個字元" pattern="[a-zA-Z0-9]{8,}" required>
+            <div class="row">
+                <input type="text" class="form-control border-info" id="name" name="name" placeholder="請輸入您的大名">
+            </div>
+            <div class="row">
+                <p id="n_p">&nbsp;</p>
+            </div>
+        </div>
+            </div>
         </div>
     </div>
     
-    <div class="form-group row mt-4">
-        <label for="password" class="col-sm-4 col-form-label text-center "><i
+    <div class="form-group row ">
+        <div class="col-6">
+            <div class="row">
+
+                <label for="password" class="col-sm-4 col-form-label text-center "><i
                 class="fas fa-mobile-alt">&nbsp;&nbsp;密碼</i></label>
-        <div class=" col-sm-8 ">
-            <input type="password" class="form-control border-info" id="password" name="password" placeholder="請輸入至少8個字元" pattern="[a-zA-Z0-9]{8,}" required>
+                <div class=" col-sm-8 ">
+                    <div class="row">
+                        <input type="password" class="form-control border-info" id="password" name="password" placeholder="請輸入至少8個字元" pattern="[a-zA-Z0-9]{8,}" >
+                    </div>
+                    <div class="row">
+                        <p id="pw_p">&nbsp;</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="row">
+
+                <label for="password" class="col-sm-4 col-form-label text-center "><i
+                        class="fas ">確認密碼</i></label>
+                <div class=" col-sm-8 ">
+                    <div class="row">
+                        <input type="password" class="form-control border-info" id="re_password"  placeholder="請輸入至少8個字元" pattern="[a-zA-Z0-9]{8,}" >
+                    </div>
+                    <div class="row">
+                    <p id="pw_rp">&nbsp;</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="form-group row mt-4">
-        <label for="email" class="col-sm-4 col-form-label text-center"><i class="fas fa-at ">&nbsp;&nbsp;信箱</i></label>
-        <input type="email" class="form-control col-sm-8 text-center border-info" id="email" name="email" placeholder="name@example.com" required>
-    </div>
-    <div class="form-group row mt-4">
-        <label for="name" class="col-sm-4 col-form-label text-center">
-            <i class="fas fa-user-tie">&nbsp;&nbsp;姓名</i></label>
-        <div class=" col-sm-8 ">
-            <input type="text" class="form-control border-info" id="name" name="name" required>
+
+    <div class="form-group row ">
+        <label for="email" class="col-sm-2 col-form-label text-center"><i class="fas fa-at ">&nbsp;&nbsp;信箱</i></label>
+        <div class="col-sm-10">
+            <div class="row">
+                <input type="email" class="form-control text-center border-info" id="email" name="email" placeholder="name@example.com" >
+            </div>
+            <div class="row">
+                <p id="e_p">&nbsp;</p>
+            </div>
         </div>
     </div>
-    <div class="form-group row mt-4">
+    <div class="form-group row ">
         <legend class="col-form-label col-sm-4 text-center"><i class="fas fa-mars-stroke">&nbsp;&nbsp;性別</i></legend>
             <div class="col-sm-8">
                 <div class="form-check-inline mr-5">  
@@ -47,14 +98,79 @@
                 </div>
             </div>
     </div>
-    <div class="form-group row mt-4">
+    <div class="form-group row ">
         <label for="birthday" class="col-sm-4 col-form-label text-center">
             <i class="fas fa-birthday-cake">&nbsp;&nbsp;生日</i></label>
         <div class="col-sm-8">
-            <input type="date" class="date" id="birthday" name="birthday" required>
+            <div class="row">
+
+                <input type="date" class="date" id="birthday" name="birthday" >
+            </div>
+            <div class="row"><p id="date_p">&nbsp;</p></div>
         </div>
         <input type="hidden"  id="note" name="note" value="">
     </div>
-    <div><input type="submit" class="btn btn-info my-5" value="確認送出"></div>
+    <div><input type="button" class="btn btn-info my-5" value="加入會員" onClick="check_date()"></div>
 </form>
 </div>
+<!-- javascrpit -->
+<script>
+    let account=document.getElementById("account");
+    let acc_p=document.getElementById("acc_p");
+    let pw_p=document.getElementById("pw_p");
+    let pw_rp=document.getElementById("pw_rp");
+    let e_p=document.getElementById("e_p");
+    let n_p=document.getElementById("n_p");
+    let date_p=document.getElementById("date_p");
+
+    function check_date(){
+        var regExp = /^[A-Za-z0-9]*$/;
+        if (regExp.test(account.value)){
+        acc_p.innerHTML = "";
+        }else{
+        acc_p.innerHTML = "<span style='color:red;'>帳號請輸入英文或數字</span>";
+        }
+        if(document.myForm.account.value.length <8){
+            acc_p.innerHTML = "<span style='color:red;'>帳號請輸入至少8個字元</span>";
+            return false;
+        }
+  
+        if(document.myForm.name.value.length == 0){
+            n_p.innerHTML = "<span style='color:red;'>【姓名】請記得填寫</span>";       
+            return false;
+        }else{
+            n_p.innerHTML = "";
+        }
+
+        if(document.myForm.password.value.length <8){
+            pw_p.innerHTML = "<span style='color:red;'>密碼請輸入至少8個字元</span>";
+            return false;
+        }else{
+            pw_p.innerHTML = "";
+        }
+
+        if(document.myForm.password.value !=document.myForm.re_password.value){
+            pw_rp.innerHTML = "<span style='color:red;'>與【密碼】輸入值不同</span>";      
+            return false;
+        }else{
+            pw_rp.innerHTML = "";
+        }
+
+        if(document.myForm.email.value.length == 0){
+            e_p.innerHTML = "<span style='color:red;'>【信箱】請記得填寫</span>";
+            return false;
+        }else{
+            e_p.innerHTML = "";
+        }
+
+        if(document.myForm.birthday.value.length == 0){
+            date_p.innerHTML = "<span style='color:red;'>請輸入【生日】</span>";
+            
+          
+            return false;
+        }
+        myForm.submit();
+    }
+
+</script>
+<!-- javascrpit end -->
