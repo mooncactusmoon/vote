bfaed1084ca436544060def44ae0286a6670ccf8
Moon <mooncactusmoon@gmail.com>
2021-12-21 20:30:04 +0800

    修改好 投票標題不重複 (已上傳220

--- api/new_subject.php
+++ api/new_subject.php
@@ -4,23 +4,34 @@ include_once "db.php";
 /**
  * 資料表的欄位名稱=>資料內容
  */
+
 $topic=['topic'=>$_POST['subject']];
-insert('topics',$topic);
+
 
 $sql="SELECT * FROM `topics` WHERE `topic`='{$_POST['subject']}'";
-$t_id=$pdo->query($sql)->fetch();
+$result=$pdo->query($sql)->fetch();
+if($result!=0){
+    echo "<script>";
+    echo "alert('投票主題已被使用過，請換主題');";
+    echo "history.back();";
+    echo "</script>";
+}else{
+    insert('topics',$topic);
+    $t_id=$pdo->query($sql)->fetch();
+    $topic_id=$t_id['id'];
+    $options=$_POST['options'];
 
-$topic_id=$t_id['id'];
-
-$options=$_POST['options'];
-
-
-foreach($options as $key => $opt){
-    $arr=['opt'=>$opt,'topic_id'=> $topic_id ];
-    insert('options',$arr);
+    foreach($options as $key => $opt){
+        $arr=['opt'=>$opt,'topic_id'=> $topic_id ];
+        insert('options',$arr);
+    }
+    //新增完之後回首頁
+    to("../index.php");
 }
-//  判斷 問卷題目有無和資料庫重複 / 選項之間是否重複
 
-//新增完之後回首頁
-to("../index.php");
+
+
+
+//  判斷 問卷題目有無和資料庫重複 
+
 ?>
