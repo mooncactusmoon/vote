9f0f25657b9b139b2e68a04a1b0a8de6416b72a8
Moon <mooncactusmoon@gmail.com>
2021-12-21 22:28:50 +0800

    調整後台 投票表列的開啟即關閉按鈕美化 (已上傳220

--- backend/manage_vote.php
+++ backend/manage_vote.php
@@ -17,6 +17,10 @@ if(isset($_GET['id'])){
     echo "<button class='btn btn-light'><a href='?do=show_vote_list&id=1'>改從舊排到新</a></button>";
 }
 //控制問卷排序方式 end
+$cou=count($array);
+echo "<input type='hidden' id='cou' value='$cou'>";
+
+
 echo "<ol class='list-group'>";
 foreach ($array as $key => $value) {
     
@@ -24,14 +28,14 @@ foreach ($array as $key => $value) {
     // ?代表當前頁面
     // 題目
     // echo "<a class='d-inline-block col-md-8' href='../index.php?do=vote&id={$value['id']}'>";
-    echo "<span class='d-inline-block col-md-6'>".$value['topic']."</span>";
+    echo "<span class='d-inline-block col-md-6 t'>".$value['topic']."</span>";
     // echo "</a>";
 
 
     // 投票開啟或關閉
     echo "<span class='d-inline-block col-md-2 '>";
     echo "<a href='../api/change_vote_status.php?id={$value['id']}'>";
-    echo ($value['sh']==1)?"開啟中":"關閉中";
+    echo ($value['sh']==1)?"<button class='sh btn btn-success btn-sm'>開啟中</button>":"<button class='sh btn btn-sm btn-secondary'>關閉中</button>";
     echo "</a></span>";
 
     //管理題目
@@ -52,3 +56,14 @@ foreach ($array as $key => $value) {
 echo "</ol>";
 
 ?>
+<!-- 嘗試做投票標題隨狀態變色(未完成) -->
+<!-- <script>
+let cou=document.getElementById("cou");
+let t=document.getElementsByClassName("t");
+let sh=document.getElementsByClassName("sh");
+    if(sh.innerHTML="開啟中"){
+    t.style = "color:black";
+    }else{
+    t.style = "color:red";
+    }
+</script> -->
